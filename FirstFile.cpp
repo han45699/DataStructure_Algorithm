0d68d3af0d269593aac799395275a1be687cb1b8
한관우 <han45699@naver.com>
2022-01-28 22:19:58 +0900

    코드 업데이트

--- FirstFile.cpp
+++ FirstFile.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 #include <vector>
+using namespace std;
+
+class Point
+{
+	int x, y;
+public:
+	Point(int a, int b) : x(a), y(b) {}
+	void SetCordinate(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+	void Print()
+	{
+		cout << "x : " << x << endl << "y : " << y << endl;
+	}
+};
+
+void foo(auto lambda_func1)
+{
+	lambda_func1();
+}
 
 int main()
 {
-
+	Point p1(1, 2);
+	
+	foo([&]() { p1.Print(); p1.SetCordinate(55, 99); });
+	
+	p1.Print();
 }
